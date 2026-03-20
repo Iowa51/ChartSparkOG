c61e946b20669192869ef8be83498d32346bd3ed
jomanwa <jomanwa@hotmail.com>
2026-03-20 15:37:11 -0400

    fix(sprint6-g4): ENCRYPTION_SALT invoked at application boot

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,8 +1,12 @@
 // This file is used to initialize Sentry on the server side
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
+import { ensureRequiredEnvVars, logEnvironmentConfig } from '@/lib/config/environment';
+
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
+    logEnvironmentConfig();
+    ensureRequiredEnvVars();
     await import('../sentry.server.config');
   }
 
