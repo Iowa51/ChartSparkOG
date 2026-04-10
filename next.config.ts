b368f1ddfa98d75249c6c284fb00ad3c74a8e99e
jomanwa <jomanwa@hotmail.com>
2026-04-10 17:28:24 -0400

    Allow microphone on all routes for clinical voice transcription

--- next.config.ts
+++ next.config.ts
@@ -82,7 +82,7 @@ const defaultHeaders = [
   defaultCSP,
   {
     key: 'Permissions-Policy',
-    value: 'camera=(), microphone=(), geolocation=()',
+    value: 'camera=(), microphone=(self), geolocation=()',
   },
 ];
 
